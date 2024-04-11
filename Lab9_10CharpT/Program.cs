f4148e39ec623113006c7c5451ad44348ac3c92e
Maxim <mudrak.maksym@chnu.edu.ua>
2024-04-11 16:48:29 +0300

    Lab10 Added

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,167 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using System;
 
+// Task1
+class InvalidTriangleException : Exception
+{
+    public InvalidTriangleException(string message) : base(message)
+    {
+    }
+}
+
+class Triangle
+{
+    private int[] sides = new int[3];
+    private string color;
+
+    public Triangle(int f, int s, int t)
+    {
+        if (!IsValidTriangle(f, s, t))
+        {
+            throw new InvalidTriangleException("Invalid triangle sides");
+        }
+
+        sides[0] = f;
+        sides[1] = s;
+        sides[2] = t;
+    }
+
+    public Triangle(int f, int s, int t, string col) : this(f, s, t)
+    {
+        color = col;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Triangle lines: a = {sides[0]}, b = {sides[1]}, c = {sides[2]}");
+    }
+
+    public int Perimeter()
+    {
+        return sides[0] + sides[1] + sides[2];
+    }
+
+    public double Area()
+    {
+        double halfPerimeter = Perimeter() / 2.0;
+        return Math.Sqrt(halfPerimeter * (halfPerimeter - sides[0]) * (halfPerimeter - sides[1]) * (halfPerimeter - sides[2]));
+    }
+
+    public int GetSide(int index)
+    {
+        if (index < 0 || index >= sides.Length)
+        {
+            throw new IndexOutOfRangeException("Index is out of range for triangle sides array");
+        }
+
+        return sides[index];
+    }
+
+    public void SetSide(int index, int newValue)
+    {
+        ValidateSide(newValue);
+        sides[index] = newValue;
+    }
+
+    public string GetColor()
+    {
+        return color;
+    }
+
+    private bool IsValidTriangle(int side1, int side2, int side3)
+    {
+        return side1 > 0 && side2 > 0 && side3 > 0 &&
+               side1 + side2 > side3 &&
+               side1 + side3 > side2 &&
+               side2 + side3 > side1;
+    }
+
+    private void ValidateSide(int side)
+    {
+        if (side <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(side), "Triangle side length must be positive");
+        }
+    }
+}
+
+// Task1
+// Task2
+
+class FacultyDayEventArgs : EventArgs
+{
+    public string EventName { get; set; }
+
+    public FacultyDayEventArgs(string eventName)
+    {
+        EventName = eventName;
+    }
+}
+
+class Faculty
+{
+    public event EventHandler<FacultyDayEventArgs> FacultyDay;
+
+    public void CelebrateFacultyDay()
+    {
+        Console.WriteLine("Faculty Day is being celebrated!");
+        OnFacultyDay(new FacultyDayEventArgs("Faculty Day"));
+    }
+
+    protected virtual void OnFacultyDay(FacultyDayEventArgs e)
+    {
+        FacultyDay?.Invoke(this, e);
+    }
+}
+
+class Student
+{
+    public Student(Faculty faculty)
+    {
+        faculty.FacultyDay += Faculty_FacultyDay;
+    }
+
+    private void Faculty_FacultyDay(object sender, FacultyDayEventArgs e)
+    {
+        Console.WriteLine($"Student is celebrating {e.EventName}!");
+    }
+}
+
+// Task2
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter task: ");
+        string? str = Console.ReadLine();
+        int n = 0;
+        if (str != null) n = int.Parse(str);
+        // Task1
+        if (n == 1)
+        {
+            try
+            {
+                Triangle invalidTriangle = new Triangle(3, 4, 8);
+            }
+            catch (InvalidTriangleException ex)
+            {
+                Console.WriteLine($"InvalidTriangleException caught: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+        // Task1
+        // Task2
+        else if (n == 2)
+        {
+            Faculty faculty = new Faculty();
+
+            Student student = new Student(faculty);
+
+            faculty.CelebrateFacultyDay();
+        }
+        // Task2
+    }
+}
